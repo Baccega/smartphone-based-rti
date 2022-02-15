b33b21272619755103f1e39e1bf214fb72e4e368
Sandro Baccega <sandro.baccega@smc.it>
2022-02-15 11:39:13 +0100

    :sparkles: [InteractiveRelighting] Add first implementation using trackbars

--- interactive_relighting.py
+++ interactive_relighting.py
@@ -1,1 +1,107 @@
-print("Interactive Relighting")
+import os
+import cv2 as cv
+import numpy as np
+from constants import INTERPOLATED_WINDOW_TITLE, SQAURE_GRID_DIMENSION
+
+from myIO import inputCoin
+from utils import getChoosenCoinVideosPaths, loadDataFile
+
+
+def nothing(x):
+    pass
+
+
+def updateFrameData(data, dirX, dirY):
+    print("Updating frame")
+    newFrame = np.zeros(
+        shape=[
+            SQAURE_GRID_DIMENSION,
+            SQAURE_GRID_DIMENSION,
+            3,
+        ],
+        dtype=np.uint8,
+    )
+    for x in range(SQAURE_GRID_DIMENSION):
+        for y in range(SQAURE_GRID_DIMENSION):
+            newFrame[x][y] = data[x][y][dirX][dirY]
+    return newFrame
+
+
+def main(interpolated_data_file_path):
+    dirX = 69
+    dirY = 172
+    prevDirX = None
+    prevDirY = None
+    frame = np.zeros(
+        shape=[
+            SQAURE_GRID_DIMENSION,
+            SQAURE_GRID_DIMENSION,
+            3,
+        ],
+        dtype=np.uint8,
+    )
+    cv.namedWindow(INTERPOLATED_WINDOW_TITLE)
+    cv.createTrackbar(
+        "x",
+        INTERPOLATED_WINDOW_TITLE,
+        dirX,
+        200,
+        nothing,
+    )
+    cv.createTrackbar(
+        "y",
+        INTERPOLATED_WINDOW_TITLE,
+        dirY,
+        200,
+        nothing,
+    )
+
+    data = loadDataFile(interpolated_data_file_path)
+
+    flag = True
+    while flag:
+        dirX = cv.getTrackbarPos("x", INTERPOLATED_WINDOW_TITLE)
+        dirY = cv.getTrackbarPos("y", INTERPOLATED_WINDOW_TITLE)
+        if prevDirX != dirX or prevDirY != dirY:
+            frame = updateFrameData(data, dirX, dirY)
+            prevDirX = dirX
+            prevDirY = dirY
+
+        cv.imshow(INTERPOLATED_WINDOW_TITLE, frame)
+
+        if cv.waitKey(1) & 0xFF == ord("q"):
+            flag = False
+
+    cv.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    coin = inputCoin()
+    (
+        not_aligned_static_video_path,
+        not_aligned_moving_video_path,
+        moving_camera_delay,
+        static_video_path,
+        moving_video_path,
+        extracted_data_file_path,
+        interpolated_data_file_path,
+    ) = getChoosenCoinVideosPaths(coin)
+
+    if not os.path.exists(interpolated_data_file_path):
+        raise (
+            Exception(
+                "You need to run the analysis before the interactive relighting on this coin!"
+            )
+        )
+
+    print(
+        "*** Interactive Relighting *** \n\tData: '{}' ".format(
+            interpolated_data_file_path
+        )
+    )
+
+    main(
+        interpolated_data_file_path,
+    )
+
+    print("All Done!")
