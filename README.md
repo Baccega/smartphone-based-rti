7d8b2d847f85f5509e5ac93fd3b176f0b9a7d241
Sandro Baccega <sandro.baccega@smc.it>
2022-03-15 17:50:13 +0100

    :memo: [Docs] Update README

--- README.md
+++ README.md
@@ -1,16 +1,53 @@
 # 🐍📷  Smartphone Based RTI
 
-Reflectance Transformation Imaging (RTI) using footage of two smartphones without requiring an expensive light dome, created in **Python** utilizing **OpenCV** .
+**Reflectance Transformation Imaging** (RTI) using footage of two smartphones without requiring an expensive light dome, created in **Python** utilizing **OpenCV** .
+
+### 🎥 Input:
+
+![sample_input](./docs/sample_input.gif)
+
+> Footage by professor Filippo Bergamasco (Ca Foscari University of Venice)
+
+### 🕹 Output (interactive):
+
+![sample_output](./docs/sample_output.gif)
+
 
 This project is the assignment for the course **Geometric and 3D Computer Vision 2020/2021**.
 
-See **[FinalProject.pdf](FinalProject.pdf)** for more details on the assignment and to download the required assets.
+See **[FinalProject.pdf](FinalProject.pdf)** for more details on the assignment and to download the required assets. 
 
-## Usage
+## 🔧 Usage
 
-Before analyzing the videos you need to get the intrinsic camera matrix from the calibration videos by running:
+Before running the scripts you need to download the required assets, the assets should include: 
+- The calibration videos for both cameras
+- The footage from the static camera
+- The footage from the moving camera
+
+You need extract them inside a new folder called `assets` in the root of the project.
+Your folder structure should look like this:
+
+![folder_structure](./docs/folder_structure.png)
+
+> If you want you can change the location of the input files by changing the corresponding row on the file `constants.py` under the heading: `ASSETS FILE NAMES AND DELAY BETWEEN FOOTAGE`.
+
+After that you can just run this commands and follow the TUI: 
 
 ```bash
-python3 camera_calibrator.py
+python3 camera_calibrator.py        # Get camera intrinsics
+python3 analysis.py                 # Get data from footage
+python3 interactive_relighting.py   # View output
 ```
+
+## ⚙️ Interpolation methods available:
+
+- **Linear RBF** (_From the scipy library_)
+- **Polinomial Texture Maps** (_Based on the homonymous paper from: Tom Malzbender, Dan Gelb, Hans Wolters_)
+## 🔬 Analysis debug modes descriptions:
+
+| #   | Mode name     | Features                                                                         |
+| --- | ------------- | -------------------------------------------------------------------------------- |
+| 0   | No debug      | -                                                                                |
+| 1   | Minimal debug | Live footage, Current light direction, marker's contours                         |
+| 2   | Full debug    | Minimal debug, Moving camera threshold, Warped moving frame, highlighted corners |
 
--- constants.py
+++ constants.py
@@ -1,9 +1,20 @@
 constants = {
-    # --- CAMERA CALIBRATION CONSTANTS
+    # --- ASSETS FILE NAMES AND DELAY BETWEEN FOOTAGE
     "CALIBRATION_CAMERA_STATIC_PATH": "assets/cam1 - static/calibration.mov",
     "CALIBRATION_CAMERA_MOVING_PATH": "assets/cam2 - moving light/calibration.mp4",
-    "CALIBRATION_INTRINSICS_CAMERA_STATIC_PATH": "data/static_intrinsics.xml",
-    "CALIBRATION_INTRINSICS_CAMERA_MOVING_PATH": "data/moving_intrinsics.xml",
+    "COIN_1_VIDEO_CAMERA_STATIC_PATH": "assets/cam1 - static/coin1.mov",
+    "COIN_1_VIDEO_CAMERA_MOVING_PATH": "assets/cam2 - moving light/coin1.mp4",
+    "COIN_2_VIDEO_CAMERA_STATIC_PATH": "assets/cam1 - static/coin2.mov",
+    "COIN_2_VIDEO_CAMERA_MOVING_PATH": "assets/cam2 - moving light/coin2.mp4",
+    "COIN_3_VIDEO_CAMERA_STATIC_PATH": "assets/cam1 - static/coin3.mov",
+    "COIN_3_VIDEO_CAMERA_MOVING_PATH": "assets/cam2 - moving light/coin3.mp4",
+    "COIN_4_VIDEO_CAMERA_STATIC_PATH": "assets/cam1 - static/coin4.mov",
+    "COIN_4_VIDEO_CAMERA_MOVING_PATH": "assets/cam2 - moving light/coin4.mp4",
+    "FILE_1_MOVING_CAMERA_DELAY": 2.724,  # [seconds] (static) 3.609 - 0.885 (moving)
+    "FILE_2_MOVING_CAMERA_DELAY": 2.024,  # [seconds] (static) 2.995 - 0.971 (moving)
+    "FILE_3_MOVING_CAMERA_DELAY": 2.275,  # [seconds] (static) 3.355 - 1.08 (moving)
+    "FILE_4_MOVING_CAMERA_DELAY": 2.015,  # [seconds] (static) 2.960 - 0.945 (moving)
+    # --- CAMERA CALIBRATION CONSTANTS
     "CHESSBOARD_SIZE": (6, 9),
     "CALIBRATION_FRAME_SKIP_INTERVAL": 40,  # We just need some, not all
     # --- ANALYSIS CONSTANTS
@@ -19,19 +30,9 @@ constants = {
     # ---  INTERACTIVE RELIGHTING CONSTANTS
     "INTERPOLATED_WINDOW_TITLE": "Interpolated Data",
     "INPUT_LIGHT_DIRECTION_WINDOW_TITLE": "Light direction input",
-    # ---  FILE NAMES CONSTANTS
-    "COIN_1_VIDEO_CAMERA_STATIC_PATH": "assets/cam1 - static/coin1.mov",
-    "COIN_1_VIDEO_CAMERA_MOVING_PATH": "assets/cam2 - moving light/coin1.mp4",
-    "COIN_2_VIDEO_CAMERA_STATIC_PATH": "assets/cam1 - static/coin2.mov",
-    "COIN_2_VIDEO_CAMERA_MOVING_PATH": "assets/cam2 - moving light/coin2.mp4",
-    "COIN_3_VIDEO_CAMERA_STATIC_PATH": "assets/cam1 - static/coin3.mov",
-    "COIN_3_VIDEO_CAMERA_MOVING_PATH": "assets/cam2 - moving light/coin3.mp4",
-    "COIN_4_VIDEO_CAMERA_STATIC_PATH": "assets/cam1 - static/coin4.mov",
-    "COIN_4_VIDEO_CAMERA_MOVING_PATH": "assets/cam2 - moving light/coin4.mp4",
-    "FILE_1_MOVING_CAMERA_DELAY": 2.724,  # [seconds] (static) 3.609 - 0.885 (moving)
-    "FILE_2_MOVING_CAMERA_DELAY": 2.024,  # [seconds] (static) 2.995 - 0.971 (moving)
-    "FILE_3_MOVING_CAMERA_DELAY": 2.275,  # [seconds] (static) 3.355 - 1.08 (moving)
-    "FILE_4_MOVING_CAMERA_DELAY": 2.015,  # [seconds] (static) 2.960 - 0.945 (moving)
+    # ---  DATA FILE NAMES CONSTANTS
+    "CALIBRATION_INTRINSICS_CAMERA_STATIC_PATH": "data/static_intrinsics.xml",
+    "CALIBRATION_INTRINSICS_CAMERA_MOVING_PATH": "data/moving_intrinsics.xml",
     "COIN_1_ALIGNED_VIDEO_STATIC_PATH": "data/1_static_aligned_video.mov",
     "COIN_1_ALIGNED_VIDEO_MOVING_PATH": "data/1_moving_aligned_video.mp4",
     "COIN_2_ALIGNED_VIDEO_STATIC_PATH": "data/2_static_aligned_video.mov",
